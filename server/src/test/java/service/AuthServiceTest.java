834ab2b9440e207a3de6921d46d7c78709926967
mikelj01 <mikelj01@byu.edu>
2026-03-13 22:15:23 -0600

    Making Test Files

---
+++ server/src/test/java/service/AuthServiceTest.java
@@ -0,0 +1,39 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Test
+    void createAuth() {
+    }
+
+    @Test
+    void getAuth() {
+    }
+
+    @Test
+    void deleteAuth() {
+    }
+
+    @Test
+    void clear() {
+    }
+    @Test
+    void createAuthFail() {
+    }
+
+    @Test
+    void getAuthFail() {
+    }
+
+    @Test
+    void deleteAuthFail() {
+    }
+
+    @Test
+    void clearFail() {
+    }
+}
---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,47 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+    @Test
+    void newGame() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void getGames() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void deleteGame() {
+    }
+
+    @Test
+    void newGameFail() {
+    }
+
+    @Test
+    void joinGameFail() {
+    }
+
+    @Test
+    void getGamesFail() {
+    }
+
+    @Test
+    void clearFail() {
+    }
+
+    @Test
+    void deleteGameFail() {
+    }
+}
