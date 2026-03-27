9688dcd6a20061912f9205a2c78e6176bd25c221
mikelj01 <mikelj01@byu.edu>
2026-03-26 20:31:08 -0600

    adding SQL auth

--- server/src/main/java/dataaccess/SQLAuthDA.java
+++ server/src/main/java/dataaccess/SQLAuthDA.java
@@ -1,11 +1,32 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.AuthData;
+import server.websocket.AuthGenerator;
+import service.AuthException;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 
 public class SQLAuthDA implements AuthDataAccess{
     @Override
     public AuthData createAuth(String userName) throws DataAccessException {
-        return null;
+        if(userName == null|| userName.isBlank()){
+            throw new AuthException("Error: Bad request");
+        }
+
+        AuthData auth = AuthGenerator.genAuth(userName);
+        var statement = "INSERT INTO pet (authToken, authData) VALUES (?, ?)";
+        String json = new Gson().toJson(auth);
+        executeUpdate(statement, auth.authToken(), json);
+        return auth;
     }
 
     @Override
@@ -21,5 +42,56 @@ public class SQLAuthDA implements AuthDataAccess{
     @Override
     public void clear() throws DataAccessException {
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof AuthData p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  Auth (
+              token VARCHAR(255) PRIMARY KEY,
+              data JSON NOT NULL,
+              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        } catch (DataAccessException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
---
+++ server/src/main/java/dataaccess/SQLUserDA.java
@@ -0,0 +1,25 @@
+package dataaccess;
+
+import model.UserData;
+
+public class SQLUserDA implements UserDataAccess{
+    @Override
+    public UserData getUser(String userName) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userName) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData addUser(UserData user) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+}
