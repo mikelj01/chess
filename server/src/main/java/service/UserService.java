ddb5f4b444ad940acafa7c3c29673ed0f3c655ca
mikelj01 <mikelj01@byu.edu>
2026-03-30 16:02:08 -0600

    geting errors right

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -27,10 +27,10 @@ public class UserService {
     }
 
     public AuthData login(LoginRequest user, String authToken) throws DataAccessException {
-        if(aServe.getAuth(authToken) != null ){
+        if (aServe.getAuth(authToken) != null) {
             throw new AuthException("Error: bad request");
         }
-        if(user.username() == null){
+        if (user.username() == null) {
             throw new UserException("Error: bad request");
         }
         try {
@@ -38,15 +38,17 @@ public class UserService {
 
             //check password with bcrypt
             boolean b = BCrypt.checkpw(user.password(), existUser.password());
-            if(existUser == null){
+            if (existUser == null) {
                 throw new AuthException("Error: There is no account with that username");
-            } else if (user.password()==null) {
+            } else if (user.password() == null) {
                 throw new UserException("Error: bad Request");
-            } else if(!b){
+            } else if (!b) {
                 throw new AuthException("Error: Incorrect Password");
             }
             AuthData userAuth = aServe.createAuth(user.username());
             return userAuth;
+        }catch (NullPointerException e){
+            throw new AuthException("Error: incorrect Password");
         }catch (UserException error){
             throw new UserException(error.getMessage());
         }catch(AuthException e){
