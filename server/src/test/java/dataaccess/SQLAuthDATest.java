a2c8b45655635906fc99191b1d7e23ecf3c47577
mikelj01 <mikelj01@byu.edu>
2026-04-15 13:53:00 -0600

    jUnit updates

--- server/src/test/java/dataaccess/SQLAuthDATest.java
+++ server/src/test/java/dataaccess/SQLAuthDATest.java
@@ -1,39 +1,111 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLAuthDATest {
 
-    @Test
-    void createAuthBad() {
+    private static SQLAuthDA adb;
+    private static AuthData existUser;
+    private static AuthData newAuth;
+    AuthData existAuth;
+
+
+    @BeforeAll
+    public static void init() throws DataAccessException {
+        adb = new SQLAuthDA();
+        existUser = new AuthData("hi", "p1");
+
+        adb.clear();
+    }
+
+    @AfterAll
+    static void stopServer() {
+        try {
+            adb.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        try {
+            adb.clear();
+            existAuth = adb.createAuth(existUser.username());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
-    void deleteAuthBad() {
+    void createAuthBad() {
+        try{
+            adb.createAuth(existUser.username());
+        } catch (DataAccessException e) {
+            assertFalse(e.getMessage().isEmpty());
+        }
     }
+
 
     @Test
     void getAuthBad() {
+        try{
+            adb.getAuth("existUser.username()");
+        } catch (DataAccessException | DBException e) {
+            assertFalse(e.getMessage().isEmpty());
+        }
     }
 
-    @Test
-    void clearBad() {
-    }
+
     @Test
     void createAuth() {
+        try{
+            AuthData auth = adb.createAuth("beans");
+            assertNotNull(auth);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Test
     void deleteAuth() {
+        try{
+            adb.deleteAuth(existAuth.authToken());
+            assertEquals(1,1);
+        } catch (DBException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void getAuth() {
+        try{
+            AuthData auth = adb.getAuth(existAuth.authToken());
+            assertNotNull(auth);
+        } catch (DBException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void clear() {
+        try{
+            adb.clear();
+            assertEquals(1, 1);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- server/src/test/java/dataaccess/SQLGameDATest.java
+++ server/src/test/java/dataaccess/SQLGameDATest.java
@@ -1,48 +1,134 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQLGameDATest {
 
+    private static GameData existingGame;
+    private static GameData newGame;
+    private static SQLGameDA gdb;
+    private static AuthData existAuth;
+    private static AuthData newAuth;
+
+
+
+    @BeforeAll
+    public static void init() throws DataAccessException {
+        gdb = new SQLGameDA();
+        ChessGame theGame = new ChessGame();
+        existAuth = new AuthData("hi", "p1");
+        newAuth = new AuthData("yo", "p2");
+        existingGame = new GameData(1, newAuth.username(), existAuth.username(), "game1", theGame);
+        newGame = new GameData(2, null, null, "game2", theGame);
+        gdb.clear();
+    }
+
+    @AfterAll
+    static void stopServer() {
+        try {
+            gdb.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    public void setup() {
+        try {
+            gdb.clear();
+            gdb.createGame(existingGame);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
     @Test
     void deleteGame() {
+        try {
+            gdb.deleteGame(1);
+            GameData game = gdb.joinGame("white", 1, existAuth);
+        } catch (DataAccessException e) {
+            assertFalse(e.getMessage().isEmpty());
+        }
     }
 
     @Test
     void createGame() {
+        try {
+            gdb.createGame(newGame);
+            ArrayList<GameData> games = gdb.listGames();
+            assertTrue(games.size()>1);
+        } catch (DataAccessException e) {
+            throw new RuntimeException("oops");
+        }
     }
 
     @Test
     void joinGame() {
+        try {
+            gdb.createGame(newGame);
+            GameData game = gdb.joinGame("BLACK", 2, existAuth);
+            assertNotNull(game);
+        }catch (DataAccessException e){
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Test
     void listGames() {
+        try{
+            ArrayList<GameData> games = gdb.listGames();
+            assertFalse(games.isEmpty());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void clear() {
+        try{
+            gdb.clear();
+            assertEquals(1, 1);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Test
-    void clearBad() {
-    }
-    @Test
-    void deleteGameBad() {
-    }
+
+    
 
     @Test
     void createGameBad() {
+        try {
+            gdb.createGame(existingGame);
+            gdb.createGame(existingGame);
+            assertEquals(1, 2);
+        } catch (DataAccessException e) {
+            assertFalse(e.getMessage().isEmpty());
+        }
     }
 
     @Test
     void joinGameBad() {
-    }
-
-    @Test
-    void listGamesBad() {
+        try {
+            gdb.createGame(newGame);
+            GameData game = gdb.joinGame("BLACK", 2, existAuth);
+            gdb.joinGame("BLACK", 2, newAuth);
+            assertNotNull(game);
+        }catch (DataAccessException e){
+            assertFalse(e.getMessage().isEmpty());
+        }
     }
 
 
